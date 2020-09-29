2b4de6ea2ecc63867631a4d8404ed3b30cd2c8c4
Dmitry Perov <Dmitry.Perov@aspose.com>
2020-09-29 10:58:42 +0300

    Updates for version 20.8.1

--- Examples/MinimalCppExample/Example.cpp
+++ Examples/MinimalCppExample/Example.cpp
@@ -8,17 +8,34 @@
 #include <MsgSaveOptions.h>
 #include <MailMessage.h>
 #include <MailAddressCollection.h>
+#include <Licensing/License.h>
 
 using namespace System;
 using namespace Aspose::Email;
+
+void LoadLicense(const String& licFile)
+{
+    try 
+    {
+        SharedPtr<License> license = System::MakeObject<License>();
+        license->SetLicense(licFile);
+    }
+    catch (Exception& e)
+    {
+        Console::WriteLine(System::String(u"Failed to load license:") + e.ToString());
+    }
+}
+
 
 int main()
 {
     Console::WriteLine(u"Example running...");
 
+    //LoadLicense(u"Aspose.Email.lic");
+
     // Create an instance of MailMessage class
     SharedPtr<MailMessage> message = System::MakeObject<MailMessage>();
-    message->set_From(u"sender@sender.com");
+    message->set_From(u"sender@sender.com"); 
     
     message->get_To()->Add(u"receiver@gmail.com");
 
