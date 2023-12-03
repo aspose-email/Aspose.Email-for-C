5e6a2a8d44933f530eb11af22ab815075967d558
Dmitri Perov <dperov2005@gmail.com>
2023-12-03 18:22:34 +0300

    Update for 23.11

--- Examples/MinimalCppExample/build_x64_debug.bat
+++ Examples/MinimalCppExample/build_x64_debug.bat
@@ -4,15 +4,17 @@
 
 set PreferredToolArchitecture=x64
 
-SET ASPOSE_ROOT=C:\project\Release\Release_23.7\Aspose.Email_for_C++_23.7
-SET ASPOSE_EMAIL_CPP_ROOT=C:\project\Release\Release_23.7\Aspose.Email_for_C++_23.7
+:SET ASPOSE_ROOT=C:\project\Release\Release_23.7\Aspose.Email_for_C++_23.7
+:SET ASPOSE_EMAIL_CPP_ROOT=C:\project\Release\Release_23.7\Aspose.Email_for_C++_23.7
+
+set ASPOSE_ROOT=C:\project\Release\CodePorting.Translator.Cs2Cpp_internal_23.11
+SET ASPOSE_EMAIL_CPP_ROOT=C:\project\net\tmp\Aspose.Email.Cpp
 
 
 
-
-rmdir /Q /S build
-mkdir build
-cd build
+rmdir /Q /S buildDeb
+mkdir buildDeb
+cd buildDeb
 cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Debug
 if %errorlevel% neq 0 goto error
 cmake --build . --config Debug
--- Examples/MinimalCppExample/build_x64_release.bat
+++ Examples/MinimalCppExample/build_x64_release.bat
@@ -2,13 +2,14 @@
 # set ASPOSE_ROOT and ASPOSE_EMAIL_CPP_ROOT environment variables to 
 # directory where content of Aspose.Email_for_C++_XX.X.zip is extracted
 
-SET ASPOSE_ROOT=C:\project\Release\Release_23.7\Aspose.Email_for_C++_23.7
-SET ASPOSE_EMAIL_CPP_ROOT=C:\project\Release\Release_23.7\Aspose.Email_for_C++_23.7
+set ASPOSE_ROOT=C:\project\Release\CodePorting.Translator.Cs2Cpp_internal_23.11
+SET ASPOSE_EMAIL_CPP_ROOT=C:\project\net\tmp\Aspose.Email.Cpp
+
 
 rmdir /Q /S build
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Debug
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release
 if %errorlevel% neq 0 goto error
 cmake --build . --config Release 
 :error
